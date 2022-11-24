e87a51edb8e7e6b9d14d8b706f4131bb8b6a6fe0
“faruk03” <dev.omarfaruk01@gmail.com>
2022-11-24 21:58:32 +0600

    adding sql in django and checking the sql(commands) in django)

--- ProjectName/AppName/urls.py
+++ ProjectName/AppName/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('submit/', submit, name='submit'),
+    path('sql_test/', sql_test, name = 'sql_test'),
 ]
--- ProjectName/AppName/views.py
+++ ProjectName/AppName/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from AppName.models import *
+
+from django.db import connection  #for connecting with sql
+from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
@@ -15,5 +19,23 @@ def submit(request):
         email = request.POST.get('email')
         t = user.objects.create(name = name, age = age, email = email)
         t.save()
-        return redirect(index)
+        return redirect(index)#--> redirects to index function
     return render(request, 'teacher/submit.html')
+
+# Search function should be implemented but I haven't understood this yet
+
+
+#Now conecting with sql --> we are checking sql(commands) in django 
+def sql_test(request):
+    cursor = connection.cursor()
+    sql = 'SELECT * FROM AppName_user'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    #print(result)--> return array oof tuple, so lets iterate it
+    for row in result:
+        print(row)
+    return HttpResponse('<h1>Hello World<h1/>')   
+    
+    
+    
+    
